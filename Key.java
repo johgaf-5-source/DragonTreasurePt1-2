8751780fb2ccc2bb09bd50254924834ef4752ba8
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-03 11:09:23 +0100

    Create Key.java

---
+++ Key.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Key extends Item {
+
+
+    @Override
+    public void use() {
+
+    }
+
+
+    public Key(String name, String itemDesc){
+        super(name, itemDesc);
+
+    }
+
+
+
+}
