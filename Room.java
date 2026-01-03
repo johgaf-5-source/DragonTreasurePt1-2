f3314fe7d9e08b9fbeadae659cea0c34b8afcfea
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-03 20:54:47 +0100

    Update Room.java

--- Room.java
+++ Room.java
@@ -2,47 +2,81 @@ import java.util.ArrayList;
 
 public class Room {
 
-// text som beskriver rummet
+
     private String roomDesc;
-    //lista med dörrarna som finns i rummet
+
     private ArrayList<Door> doors = new ArrayList<>();
-// konstruktor som skapar ett rum med en beskrivning
+    private ArrayList<Item> items = new ArrayList<>();
+
+
+
     public Room(String roomDesc) {
         this.roomDesc = roomDesc;
 
     }
 
-    //setter för rumsbeskrivningen, kan användas om man vill ändra på beskrivningen under spelets gång
+/*
     public void setRoomDesc(String roomDesc) {
         this.roomDesc = roomDesc;
-    }
-//getter för rumsbeskrivningen, returnerar beskrivningen när spelaren går in i rummet
+    }*/
+
     public String getRoomDesc() {
 
         return this.roomDesc;
     }
-// lägger till en ny dörr till rummet
+
+    public Door getDoor(char position){
+        for (Door door : doors) {
+            if (door.getPositionChar() == position) {
+                return door;
+            }
+        }
+        return null;
+    }
+
+
     public void addDoor(Door door) {
         doors.add(door);
     }
 
-//returnerar listan med dörrarna till rummet
+
     public ArrayList<Door> getDoors() {
         return doors;
     }
 
-//skriver ut tillhörande beskrivningar till nuvarande rum med tillhörande dörrar
-    public void doNarrative(Room currentRoom) {
-
-        System.out.println(currentRoom.getRoomDesc());
-
-//loopar igenom alla dörrar i rummet och skriver ut deras beskrivningar
-        for (Door d : currentRoom.getDoors()) {
-            System.out.println(d.getPositionDesc());
+    public Item getItem(String name){
+        for (Item item : items) {
+            if (item.getName().equals(name)) {
+                return item;
+            }
         }
+        return null;
+    }
 
-
+    public void addItem(Item item) {
+        items.add(item);
+    }
+    public ArrayList<Item> getItems() {
+        return items;
     }
 
 
+
+    public void doNarrative(Room currentRoom) {
+
+
+
+        System.out.println(currentRoom.getRoomDesc());
+
+        for (Item i : currentRoom.getItems()){
+            if(i.getExistance()){
+            System.out.println(i.getItemDesc());
+            }
+
+            }
+
+        for (Door d : currentRoom.getDoors()) {
+            System.out.println(d.getPositionDesc());
+        }
+    }
 }
