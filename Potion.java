8534814be83aeb9ffd4058f46ad1d2e373c8d485
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-06 11:24:23 +0100

    Update Potion.java

--- Potion.java
+++ Potion.java
@@ -1,19 +1,36 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Potion extends Item {
     private int healing;
+Scanner scanner = new Scanner(System.in);
+
 
 
 
 @Override
-void use(ArrayList<Room> rooms) {
+void use(ArrayList<Room> rooms, Player newPlayer) {
+    System.out.println("Du tog upp hälsodrycken.");
+    if ((newPlayer.getHealthPoints()) < 10 ){
+        System.out.println("Du har "+ newPlayer.getHealthPoints() +" hälsopoäng kvar. Kan vara en bra idé att dricka den där hälsodrycken [d]");
+        char heal = scanner.next().charAt(0);
+        if (heal == 'd'){
+            newPlayer.setHealthPoints(newPlayer.getHealthPoints() + healing);
+            System.out.println("Du dricker hälsodrycken och återfår "+ healing +" hälsopoäng.");
+            }
 
-}
+        }
+    }
 
 
-    public Potion(String name, String itemDesc, boolean exists, int healing){
+
+
+    public Potion(String name, String itemDesc, boolean exists, int healing) {
         super(name, itemDesc, exists);
         this.healing = healing;
+
+
+
     }
 
     public void setHealing(int healing){
@@ -27,3 +44,4 @@ void use(ArrayList<Room> rooms) {
 
 
 }
+
