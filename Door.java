fdd3b869728520ee9813526e4081502bc2bab92f
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-08 21:20:24 +0100

    Update Door.java

--- Door.java
+++ Door.java
@@ -6,10 +6,17 @@ public class Door {
     // true om dörrens visas som låst, false om dörren visas som öppen
     private boolean locked;
 
+    private int doorId;
+
     // konstruktor som skapar en dörr med position och låst-status
-    public Door(char position, boolean locked) {
-        setPosition(position);
-        setLocked(locked);
+    public Door(char position, boolean locked, int doorId) {
+        this.position = position;
+        this.locked = locked;
+        this.doorId = doorId;
+    }
+
+    public int getDoorId() {
+        return doorId;
     }
 
 //setter för position, kan användas om man vill ändra riktning senare
@@ -28,7 +35,6 @@ public class Door {
             case 'n' -> locked
                     ? "Du ser en låst dörr norr om dig [n]" //om dörren är låst
                     : "Du kan gå norrut [n]"; //om dörren är öppen
-
             case 's' -> locked
                     ? "Du ser en låst dörr i söder [s]"
                     : "Du kan gå söderut [s]";
@@ -39,8 +45,22 @@ public class Door {
                     ? "Du ser en låst dörr i väster [v]"
                     : "Du kan gå västerut [v]";
 
+//inte jätte nödvändigt egentligen, men det finns ifall man skulle vilja ändra hur dörrarna läggs ut
+            case 'N' -> locked
+                    ? "Du ser en låst utgång norrut [n]"
+                    : "Du ser en utgång norrut [n]";
+            case 'S' -> locked
+                    ? "Du ser en låst utgång söderut [s]"
+                    : "Du ser en utgång söderut [s]";
+            case 'Ö' -> locked
+                    ? "Du ser en låst utgång österut [ö]"
+                    : "Du ser en utgång österut [ö]";
+            case 'V' -> locked
+                    ? "Du ser en låst utgång västerut [v]"
+                    : "Du ser en utgång västerut [v]";
 
-            default -> "Du ser en utgång österut [ö]"; //om positionen inte matchar
+            default -> throw new IllegalStateException("Unexpected position: " + position);
+
 
         };
 
@@ -49,11 +69,9 @@ public class Door {
     public void setLocked(boolean locked) {
         this.locked = locked;
     }
+
 //returnerar true om dörren är låst, false om den går att passera
     public boolean isLocked() {
-
         return locked;
     }
-// test för ändring i main branch
-
 }
