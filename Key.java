6462e40662c1864d8b3d3ca8b29fb3e5fa245ebf
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-08 21:26:22 +0100

    Create Key.java

---
+++ Key.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Key extends Item {
+
+private int keyId;
+
+    @Override
+     void use(ArrayList<Room> rooms, Player newPlayer) {
+
+      //  System.out.println("Du plockar upp nyckeln.");
+        
+        for(Room room : rooms){
+            for (Door door : room.getDoors()) {
+                if(door.isLocked() && door.getDoorId() == keyId){
+                    door.setLocked(false);
+                    return;
+                }
+            }
+        }
+
+
+            }
+
+
+
+
+    public Key(String name, String itemDesc, int keyId) {
+        super(name, itemDesc);
+        this.keyId = keyId;
+    }
+
+
+
+}
