e29c04b26f8310868cb1880e489f55a0e9f06de9
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-08 21:23:33 +0100

    Create Treasure.java

---
+++ Treasure.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Treasure extends Item{
+
+    private int goldValue;
+
+
+    public Treasure(String name, String itemDesc, int goldValue){
+      super(name, itemDesc);
+      this.goldValue = goldValue;
+    }
+
+    public void setGoldValue(int goldValue){
+        this.goldValue = goldValue;
+    }
+
+    public int getGoldValue(){
+        return goldValue;
+    }
+    @Override
+    void use(ArrayList<Room> rooms, Player newPlayer) {
+        
+    }
+}
