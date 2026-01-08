9bca71f45b07f950caa85d0d45658a42842d8161
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-08 21:20:57 +0100

    Update Dungeon.java

--- Dungeon.java
+++ Dungeon.java
@@ -17,10 +17,10 @@ public class Dungeon {
     }
 
     // setter för aktuellt rum
-    public void setCurrentRoom(Room currentRoom) {
+   /* public void setCurrentRoom(Room currentRoom) {
         this.currentRoom = currentRoom;
 
-    }
+    } */
 
     //getter för aktuellt rum
     public Room getCurrentRoom() {
@@ -29,17 +29,18 @@ public class Dungeon {
 
     //konstruktor för dungeon
     public Dungeon(String welcomeMessage, Room currentRoom) {
-        setWelcomeMessage(welcomeMessage);
-        setCurrentRoom(currentRoom);
+        this.welcomeMessage = welcomeMessage;
+        this.currentRoom = currentRoom;
 
     }
 
-    // Huvudmetod som styr när spelaren in i grottan
-    public void playGame(ArrayList<Room> rooms) {
-        DragonTreasure close = new DragonTreasure();
+    // Huvudmetod som styr när spelaren är inne i grottan
+    public void playGame(ArrayList<Room> rooms, Player newPlayer) {
+        DragonTreasure show = new DragonTreasure();
 
 
-        Room narrateRoom = new Room(" ");
+        Room narrateRoom = new Room(" ", false);
+
 
         Scanner scanner = new Scanner(System.in);
 
@@ -48,59 +49,155 @@ public class Dungeon {
         //visar beskrivning av det aktuella rummet och vilka dörrar som finns
         narrateRoom.doNarrative(currentRoom);
 
+
 //loop som körs tills spelet avslutas
         while (true) {
-            char direction = scanner.next().charAt(0);
+            char action = scanner.next().charAt(0);
+            Door door;
+
+            door = currentRoom.getDoor(action);
+
+            if (door != null && door.isLocked()) {
+                show.doorLocked();
+                continue;
+
+            }
+
+
             // kollar vilket rum spelaren befinner sig i och hanterar rörelser från spelaren
             if (currentRoom == rooms.get(0)) {
-                if (direction == 'n') {
-                    setCurrentRoom(rooms.get(1));
-                } else if (direction == 's') {
-                    setCurrentRoom(rooms.get(4));
-                }
-            } else if (currentRoom == rooms.get(1)) {
-                if (direction == 'ö') {
-                    setCurrentRoom(rooms.get(2));
-                } else if (direction == 's') {
-                    setCurrentRoom(rooms.get(0));
+                switch (action) {
+                    case 'n' -> currentRoom = rooms.get(1);
+                    case 's' -> currentRoom = rooms.get(4);
                 }
 
+
+            } else if (currentRoom == rooms.get(1)) {
+                switch (action) {
+                    case 'ö' -> currentRoom = rooms.get(2);
+                    case 's' -> currentRoom = rooms.get(0);
+                }
+
+
             } else if (currentRoom == rooms.get(2)) {
-                if (direction == 's') {
-                    setCurrentRoom(rooms.get(3));
-                } else if (direction == 'v') {
-                    setCurrentRoom(rooms.get(1));
-                } else if (direction == 'ö') {
-                    close.endGame(); //skriver ut sluttmeddelande
-                    return; //avslutar playGame-metoden och loopen
+                switch (action) {
+                    case 's' -> currentRoom = rooms.get(3);
+                    case 'v' -> currentRoom = rooms.get(1);
+                    case 'ö' -> {
+                        show.endGame(newPlayer);
+                        return;
+                    }
+
                 }
 
             } else if (currentRoom == rooms.get(3)) {
-                if (direction == 'v') {
-                    setCurrentRoom(rooms.get(4));
-                } else if (direction == 'n') {
-                    setCurrentRoom(rooms.get(2));
-                } else if (direction == 'ö') {
-                    close.chest();
+                switch (action) {
+                    case 'v' -> currentRoom = rooms.get(4);
+                    case 'n' -> currentRoom = rooms.get(2);
+                    case 'ö' -> currentRoom = rooms.get(5);
                 }
 
+
             } else if (currentRoom == rooms.get(4)) {
-                if (direction == 'n') {
-                    setCurrentRoom(rooms.get(0));
-                } else if (direction == 'ö') {
-                    setCurrentRoom(rooms.get(3));
+                switch (action) {
+                    case 'n' -> currentRoom = rooms.get(0);
+                    case 'ö' -> currentRoom = rooms.get(3);
+
+                }
+            } else if (currentRoom == rooms.get(5)) {
+                switch (action) {
+                    case 'v' -> currentRoom = rooms.get(3);
                 }
             }
 
-// efter spelaren rört sig, visa rummet och tillgängliga dörrar
-            narrateRoom.doNarrative(currentRoom);
+            for (Item i : new ArrayList<>(currentRoom.getItems())) {
+                if (action == 'p') {
+                    switch (i.getName()) {
+                        case "key" -> System.out.println("Du tog upp nyckeln.");
+                        case "sword" -> System.out.println("Du tog upp svärdet.");
+                        case "potion" -> System.out.println("Du tog upp hälsodrycken");
+                    }
+                    if (i.getName().equals("potion") && newPlayer.getHealthPoints() < 10) {
+                        System.out.println("Du har " + newPlayer.getHealthPoints() + " hälsopoäng kvar. Kan vara en bra idé att dricka den där hälsodrycken [d]");
+                        action = scanner.next().charAt(0);
+                    }
 
+                    currentRoom.removeItem(i);
+                    newPlayer.addItem(i);
+                    if (!i.getName().equals("potion") && !i.getName().equals("treasure")) {
+                        i.use(rooms, newPlayer);
+                    }
+                } else if (i.getName().equals("treasure")) {
+
+
+
+                        newPlayer.addItem(i);
+                        newPlayer.addItem(i);
+                        currentRoom.removeItem(i);
+
+                }
+                break;
+            }
+            if (action == 'd') {
+                Item potion = newPlayer.getItemByName("potion");
+                if (potion != null) {
+                    potion.use(rooms, newPlayer);
+                } else {
+                    System.out.println("Du har ingen hälsodryck");
+                }
+            }
+
+            for (Monster m : currentRoom.getMonsters()) {
+
+                if (newPlayer.getDamage() > 1) {
+                    newPlayer.setPlayerDesc("Du attacerar " + m.getName() + " och gör " + newPlayer.getDamage() + " skador");
+                } else {
+                    newPlayer.setPlayerDesc("Du attacerar " + m.getName() + " och gör " + newPlayer.getDamage() + " skada");
+                }
+
+                narrateRoom.doNarrative(currentRoom);
+                while (newPlayer.getHealthPoints() > 0 || m.getHealthPoints() > 0) {
+
+                    narrateRoom.doBattle(currentRoom, newPlayer);
+
+                    newPlayer.setHealthPoints(newPlayer.getHealthPoints() - m.getDamage());
+                    m.setHealthPoints(m.getHealthPoints() - newPlayer.getDamage());
+                    if (m.getHealthPoints() == 0) {
+
+                        currentRoom.removeMonster(m);
+
+
+                        currentRoom.setBattle(false);
+
+                        newPlayer.setPlayerDesc("Du besegrar " + m.getName().toLowerCase());
+                        System.out.println(newPlayer.getPlayerDesc());
+
+
+                        narrateRoom.doBattle(currentRoom, newPlayer);
+                        currentRoom.setRoomDesc("");
+                        break;
+
+                    } else if (newPlayer.getHealthPoints() == 0) {
+                        break;
+                    }
+
+
+                }
+                narrateRoom.doBattle(currentRoom, newPlayer);
+                break;
+            }
+
+
+            if (newPlayer.getHealthPoints() == 0) {
+                show.endGame(newPlayer);
+                break;
+            }
+
+            narrateRoom.doNarrative(currentRoom);
+            //  narrateRoom.doBattle(currentRoom, newPlayer);
         }
 
     }
 }
-
-
-
 
 
