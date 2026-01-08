4da36ee2fcce2ed6de8d866fbd36875c4970ca3f
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-08 21:21:56 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
+
 public class Player {
     // spelarens namn
     private String name;
+    private int healthPoints;
+    private int damage;
+    private final ArrayList<Item> inventory;
+    private String playerDesc;
 
-    //konstruktor som skapar en spelare med namn
-    public Player (String name) {
+
+    public Player(String name, int healthPoints, int damage, ArrayList<Item> inventory, String playerDesc) {
         this.name = name;
+        this.healthPoints = healthPoints;
+        this.damage = damage;
+        this.inventory = inventory;
+        this.playerDesc = playerDesc;
+
     }
 
     //setter metod för att ändra spelarens namn
     //används när spelaren skriver sitt namn i början av spelet med hjälp av scanner
-    public void setName (String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
@@ -17,5 +28,53 @@ public class Player {
     public String getName() {
         return name;
     }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+
+    public void addItem(Item item) {
+        inventory.add(item);
+    }
+    public void removeItem(Item item) {
+        inventory.remove(item);
+    }
+
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
+
+    public void setPlayerDesc(String playerDesc) {
+        this.playerDesc = playerDesc;
+    }
+    public String getPlayerDesc() {
+        return playerDesc;
+    }
+
+    public Item getItemByName(String name) {
+        for (Item item : inventory) {
+            if (item.getName().equalsIgnoreCase(name)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+
+
 
 }
