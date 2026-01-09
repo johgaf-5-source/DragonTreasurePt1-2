e693fd8cdba49116829f41cc0d8e361a75831bac
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-09 14:00:38 +0100

    Update Treasure.java

--- Treasure.java
+++ Treasure.java
@@ -2,23 +2,18 @@ import java.util.ArrayList;
 
 public class Treasure extends Item{
 
-    private int goldValue;
 
 
-    public Treasure(String name, String itemDesc, int goldValue){
+
+    public Treasure(String name, String itemDesc){
       super(name, itemDesc);
-      this.goldValue = goldValue;
+
     }
 
-    public void setGoldValue(int goldValue){
-        this.goldValue = goldValue;
-    }
 
-    public int getGoldValue(){
-        return goldValue;
-    }
+
     @Override
     void use(ArrayList<Room> rooms, Player newPlayer) {
-        
+
     }
 }
