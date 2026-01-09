54a6c6c035fba5fd5e2421c815057564f10bd5df
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-09 13:57:49 +0100

    Update Dungeon.java

--- Dungeon.java
+++ Dungeon.java
@@ -16,11 +16,7 @@ public class Dungeon {
         return this.welcomeMessage;
     }
 
-    // setter för aktuellt rum
-   /* public void setCurrentRoom(Room currentRoom) {
-        this.currentRoom = currentRoom;
 
-    } */
 
     //getter för aktuellt rum
     public Room getCurrentRoom() {
@@ -35,19 +31,19 @@ public class Dungeon {
     }
 
     // Huvudmetod som styr när spelaren är inne i grottan
-    public void playGame(ArrayList<Room> rooms, Player newPlayer) {
+    public void playGame(ArrayList<Room> rooms, Player newPlayer, ArrayList<Item> items) {
         DragonTreasure show = new DragonTreasure();
 
-
+// ett objekt av Room för att kunna styra när och hur ett narrativ skrivs ut
         Room narrateRoom = new Room(" ", false);
 
-
+// spelet tar emot inmatning från spelaren för att kunna navigera mellan rum, eller plocka upp föremål
         Scanner scanner = new Scanner(System.in);
 
 
         System.out.println("När du går in i grottan kollapsar ingången bakom dig.");
         //visar beskrivning av det aktuella rummet och vilka dörrar som finns
-        narrateRoom.doNarrative(currentRoom);
+        narrateRoom.doNarrative(currentRoom, newPlayer);
 
 
 //loop som körs tills spelet avslutas
@@ -57,12 +53,13 @@ public class Dungeon {
 
             door = currentRoom.getDoor(action);
 
+            // kollar om ett rum har en dörr, och om dörren är låst
+            // om dörren är låst skrivs ett meddelande ut att det inte går att passera dörren, istället för att byta rum till ett nytt rum
             if (door != null && door.isLocked()) {
                 show.doorLocked();
                 continue;
 
             }
-
 
             // kollar vilket rum spelaren befinner sig i och hanterar rörelser från spelaren
             if (currentRoom == rooms.get(0)) {
@@ -84,7 +81,7 @@ public class Dungeon {
                     case 's' -> currentRoom = rooms.get(3);
                     case 'v' -> currentRoom = rooms.get(1);
                     case 'ö' -> {
-                        show.endGame(newPlayer);
+                        show.endGame(newPlayer, items);
                         return;
                     }
 
@@ -110,6 +107,8 @@ public class Dungeon {
                 }
             }
 
+
+            // för varje föremål i ett rum (förutom skatten) kan spelaren plocka upp med 'p' vilket tar bort föremålet från rummet och läggs till i spelaren
             for (Item i : new ArrayList<>(currentRoom.getItems())) {
                 if (action == 'p') {
                     switch (i.getName()) {
@@ -124,20 +123,22 @@ public class Dungeon {
 
                     currentRoom.removeItem(i);
                     newPlayer.addItem(i);
+                    // alla föremål förutom hälsodryck och skatten används direkt
                     if (!i.getName().equals("potion") && !i.getName().equals("treasure")) {
                         i.use(rooms, newPlayer);
-                    }
+                    } // om rummet innehåller en skatt plockas den upp automatiskt, och läggs in i spelaren
                 } else if (i.getName().equals("treasure")) {
 
-
-
                         newPlayer.addItem(i);
-                        newPlayer.addItem(i);
-                        currentRoom.removeItem(i);
-
+                        // så länge rummet inte är i strid tas föremålet bort, för att kunna visa att spelaren plockar upp skatten i narrativet
+                       if (!currentRoom.inBattle()) {
+                           currentRoom.removeItem(i);
+                       }
                 }
                 break;
             }
+
+            // för att använda hälsodryckens effekt måste spelaren mata in 'd' på tangentbordet
             if (action == 'd') {
                 Item potion = newPlayer.getItemByName("potion");
                 if (potion != null) {
@@ -148,56 +149,59 @@ public class Dungeon {
             }
 
             for (Monster m : currentRoom.getMonsters()) {
-
+// liten if-sats för att bestämma meddelande beroende på hur mycket skada spelaren gör, en skada i singular och flera skador i plural
                 if (newPlayer.getDamage() > 1) {
                     newPlayer.setPlayerDesc("Du attacerar " + m.getName() + " och gör " + newPlayer.getDamage() + " skador");
                 } else {
                     newPlayer.setPlayerDesc("Du attacerar " + m.getName() + " och gör " + newPlayer.getDamage() + " skada");
                 }
 
-                narrateRoom.doNarrative(currentRoom);
+                narrateRoom.doNarrative(currentRoom, newPlayer);
                 while (newPlayer.getHealthPoints() > 0 || m.getHealthPoints() > 0) {
-
+// loopar igenom strids narrativet tills spelaren eller monstret är besegrat
                     narrateRoom.doBattle(currentRoom, newPlayer);
 
                     newPlayer.setHealthPoints(newPlayer.getHealthPoints() - m.getDamage());
                     m.setHealthPoints(m.getHealthPoints() - newPlayer.getDamage());
                     if (m.getHealthPoints() == 0) {
-
+// monstret tas bort från rummet
                         currentRoom.removeMonster(m);
-
-
+// rummet är inte längre i slagsmål
                         currentRoom.setBattle(false);
-
+// meddelande om spelaren besegrar monstret
                         newPlayer.setPlayerDesc("Du besegrar " + m.getName().toLowerCase());
                         System.out.println(newPlayer.getPlayerDesc());
 
 
+// beskrivningen av själva rummet tas bort, för att få narrativet låta lite mer "naturligt"
                         narrateRoom.doBattle(currentRoom, newPlayer);
                         currentRoom.setRoomDesc("");
+                        // while-loopen avslutas
                         break;
 
                     } else if (newPlayer.getHealthPoints() == 0) {
+                        // while-loopen avslutas
                         break;
                     }
 
 
                 }
                 narrateRoom.doBattle(currentRoom, newPlayer);
+                // for-loopen avslutas
                 break;
             }
 
 
             if (newPlayer.getHealthPoints() == 0) {
-                show.endGame(newPlayer);
+                // når spelaren 0 hälso-poäng skrivs ett slut-meddelande ut, och while (true)-loopen avslutas, vilket avslutar playgame, vilket avslutar spelet
+                show.endGame(newPlayer, items);
                 break;
             }
 
-            narrateRoom.doNarrative(currentRoom);
-            //  narrateRoom.doBattle(currentRoom, newPlayer);
+            narrateRoom.doNarrative(currentRoom, newPlayer);
+           
         }
 
     }
 }
-
 
