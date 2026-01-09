c31d3437d981df28c85e4580537d117303e56593
johgaf-5-source <johgaf-5@student.ltu.se>
2026-01-09 13:58:14 +0100

    Update Item.java

--- Item.java
+++ Item.java
@@ -4,19 +4,12 @@ public abstract class Item {
     private String name;
     private String itemDesc;
 
-    // kommer antagligen tas bort, då den inte fyller någon funktion än så länge
-   // private boolean exists;
 
     public Item(String name, String itemDesc) {//, boolean exists) {
         this.name = name;
         this.itemDesc = itemDesc;
-      //  this.exists = exists;
-
-    }
 
 
-    public void setName(String name) {
-        this.name = name;
     }
 
 
@@ -24,37 +17,25 @@ public abstract class Item {
         return name;
     }
 
-    public void setItemDesc(String itemDesc) {
-        this.itemDesc = itemDesc;
-    }
-
     public String getItemDesc() {
         return switch (name) {
-            case "key" -> "Du ser en nyckel som ligger på golvet, du kan ta upp den [p]";
 
+            // beskriver vilka föremål som finns i rummen
+            case "key" -> "Du ser en nyckel som ligger på golvet, du kan ta upp den [p]";
 
             case "potion" -> "Du ser en hälsodryck på golvet, du kan plocka upp den [p]";
 
-
             case "sword" -> "Du ser ett svärd på golvet, du kan ta upp det [p]";
 
+// här kunde skatten fungerat på samma sätt som de andra, men tanken här var att spelaren direkt plockar upp skatten automatiskt när den får tillgång till den, vilket i detta fallet sker
             case "treasure" -> "Du samlar skatten";
-
-
 
             default -> "";
 
         };
     }
-/*
-    public void setExisting(boolean exists) {
-        this.exists = exists;
-    }
 
-    public boolean isExisting() {
-        return exists;
-    }
-*/
+    // abstrakt metod för hur föremål kan användas
     abstract void use(ArrayList<Room> rooms, Player newPlayer);
 
 
